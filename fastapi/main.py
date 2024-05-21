c0f87e8a58333414e793f9bc866b06dbbf96f2ab
Faina Gorodkova <kisik.murr@gmail.com>
2024-05-21 11:17:40 +0300

    закомментировала ошибку в мейне

--- fastapi/main.py
+++ fastapi/main.py
@@ -52,7 +52,7 @@ def read_users(skip: int = 0,
                limit: int = 100,
                db: Session = Depends(get_db),
                ads_id: Annotated[str | None, Cookie()] = None,
-               # token: Annotated[str, Depends(oauth2_scheme)]):
+               # token: Annotated[str, Depends(oauth2_scheme)]) :
                ):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users, {"ads_id": ads_id}
