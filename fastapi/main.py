a6700ef08113bb99bbccfb131bb44bc252f0cf5a
chilofell <kisik.murr@gmail.com>
2024-06-05 13:04:55 +0700

    поиск пользователя по почте и паролю

--- fastapi/main.py
+++ fastapi/main.py
@@ -60,9 +60,9 @@ def read_users(skip: int = 0,
     return users, {"ads_id": ads_id} #"token": token}
 
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+@app.get("/users/{user_email}")
+def read_user(user_email: str, user_password: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_email=user_email, user_password=user_password)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
