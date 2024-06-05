e7b8156cb7091fb88bb5c95adf277b1b61cdb356
00rei <wiiuxnx@gmail.com>
2024-06-05 11:34:19 +0700

    Revert "добавлена авторизация при помощи fastapi users"
    This reverts commit d9ce679ad2f23d31f1a7be90e97f25ed5170a344.

--- fastapi/models.py
+++ fastapi/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import  Column, ForeignKey, Integer, String, Time
+from sqlalchemy import Column, ForeignKey, Integer, String, Time
 from sqlalchemy.orm import relationship
 from database import Base
 
